2b62bd40d253d8c0e4587a26d2436a596504df67
Łukasz Hołowacz <Lukasz.holowacz5@gmail.com>
2023-05-22 14:31:22 +0200

    Joystick kurwa znowu

--- My project/Assets/Scripts/Player/Player.cs
+++ My project/Assets/Scripts/Player/Player.cs
@@ -15,13 +15,21 @@ public class Player : MonoBehaviour
     // Update is called once per frame
     void FixedUpdate()
     {
-        if (movementJoystick.joystickVec.y != 0)
+        // Odczytaj wartoœci ruchu z joysticka
+        float horizontalMovement = movementJoystick.joystickVec.x;
+        float verticalMovement = movementJoystick.joystickVec.y;
+
+        // Oblicz kierunek ruchu
+        Vector2 movementDirection = new Vector2(horizontalMovement, verticalMovement).normalized;
+
+        // Ustaw prêdkoœæ poruszania siê postaci
+        rb.velocity = movementDirection * playerSpeed;
+
+        // Obracanie postaci wokó³ w³asnej osi
+        if (movementDirection != Vector2.zero)
         {
-            rb.velocity = new Vector2(movementJoystick.joystickVec.x * playerSpeed, movementJoystick.joystickVec.y * playerSpeed);
-        }
-        else
-        {
-            rb.velocity = Vector2.zero;
+            float rotationAngle = -Mathf.Atan2(movementDirection.x, movementDirection.y) * Mathf.Rad2Deg;
+            transform.rotation = Quaternion.AngleAxis(rotationAngle, Vector3.forward);
         }
     }
 }
